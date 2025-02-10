8dae1d0e3a2f22e48ccb9168d814944342a058c3
Tilak Suthar <44789847+tiks98@users.noreply.github.com>
2025-02-10 16:25:19 -0500

    animation added home page sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				slideInLeft: {
+					"0%": { opacity: 0, transform: "translateX(-100%)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+				slideInRight: {
+					"0%": { opacity: 0, transform: "translateX(100%)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+			},
+			animation: {
+				slideInLeft: "slideInLeft 2s ease-out forwards",
+				slideInRight: "slideInRight 2s ease-out forwards",
+			},
+		},
 	},
 	plugins: [],
 };
